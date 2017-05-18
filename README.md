3259990f56ef2a50ef6794d38f24ea2d1b104d20
v-fexue <v-fexue@V-FEXUE-W10>
2017-05-18 13:59:08 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,71 @@
+---
+services: active-directory
+platforms: dotnet
+author: dstrockis
+---
+
+# Integrating Azure AD into an ASP.NET Core web app
+This sample shows how to build a .Net MVC web application that uses OpenID Connect to sign-in users from a single Azure Active Directory tenant, using the ASP.Net Core OpenID Connect middleware.
+
+For more information about how the protocols work in this scenario and other scenarios, see [Authentication Scenarios for Azure AD](http://go.microsoft.com/fwlink/?LinkId=394414).
+
+> This sample has been updated to ASP.NET Core 1.0.  Looking for previous versions of this code sample? Check out the tags on the [releases](../../releases) GitHub page.
+
+## How To Run This Sample
+
+Getting started is simple!  To run this sample you will need:
+- Install .NET Core for Windows by following the instructions at [dot.net/core](https://dot.net/core), which will include Visual Studio 2015 Update 3.
+- An Internet connection
+- An Azure Active Directory (Azure AD) tenant. For more information on how to get an Azure AD tenant, please see [How to get an Azure AD tenant](https://azure.microsoft.com/en-us/documentation/articles/active-directory-howto-tenant/) 
+- A user account in your Azure AD tenant. This sample will not work with a Microsoft account, so if you signed in to the Azure portal with a Microsoft account and have never created a user account in your directory before, you need to do that now.
+
+### Step 1:  Clone or download this repository
+
+From your shell or command line:
+
+`git clone https://github.com/Azure-Samples/active-directory-dotnet-webapp-openidconnect-aspnetcore.git`
+
+### Step 2:  Register the sample with your Azure Active Directory tenant
+
+1. Sign in to the [Azure portal](https://portal.azure.com).
+2. On the top bar, click on your account and under the **Directory** list, choose the Active Directory tenant where you wish to register your application.
+3. Click on **More Services** in the left hand nav, and choose **Azure Active Directory**.
+4. Click on **App registrations** and choose **Add**.
+5. Enter a friendly name for the application, for example 'WebApp-OpenIDConnect-DotNet' and select 'Web Application and/or Web API' as the Application Type. For the sign-on URL, enter the redirect URI for the sample, which is by default `https://localhost:44353/signin-oidc`. Click on **Create** to create the application.
+6. While still in the Azure portal, choose your application, click on **Settings** and choose **Properties**.
+7. Find the Application ID value and copy it to the clipboard.
+8. In the same page, change the `logoutUrl` property to `https://localhost:44353/Account/EndSession`.  This is the default single sign out URL for this sample.
+
+### Step 3:  Configure the sample to use your Azure Active Directory tenant
+
+1. Open the solution in Visual Studio 2015.
+2. Open the `config.json` file.
+3. Find the `Tenant` property and replace the value with your AAD tenant name.
+4. Find the `ClientId` and replace the value with the Application ID from the Azure portal.
+5. If you changed the base URL of the sample, find the app key `ida:PostLogoutRedirectUri` and replace the value with the new base URL of the sample.
+
+### Step 4:  Run the sample
+
+Clean the solution, rebuild the solution, and run it.
+
+Click the sign-in link on the homepage of the application to sign-in.  On the Azure AD sign-in page, enter the name and password of a user account that is in your Azure AD tenant.
+
+## About The Code
+
+This sample shows how to use the OpenID Connect ASP.Net Core middleware to sign-in users from a single Azure AD tenant.  The middleware is initialized in the `Startup.cs` file, by passing it the Client ID of the application and the URL of the Azure AD tenant where the application is registered.  The middleware then takes care of:
+- Downloading the Azure AD metadata, finding the signing keys, and finding the issuer name for the tenant.
+- Processing OpenID Connect sign-in responses by validating the signature and issuer in an incoming JWT, extracting the user's claims, and putting them on ClaimsPrincipal.Current.
+- Integrating with the session cookie ASP.Net Core middleware to establish a session for the user. 
+
+You can trigger the middleware to send an OpenID Connect sign-in request by decorating a class or method with the `[Authorize]` attribute, or by issuing a challenge,
+```C#
+await HttpContext.Authentication.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/" });
+```
+Similarly you can send a signout request,
+```C#
+await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+```
+When a user is signed out, they will be redirected to the `Post_Logout_Redirect_Uri` specified when the OpenID Connect middleware is initialized.
+
+All of the middleware in this project is created as a part of the open source [Asp.Net Security](https://github.com/aspnet/Security) project.
---
+++ WebApp-OpenIDConnect-DotNet/Startup.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using System.Collections;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApp_OpenIDConnect_DotNet
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add MVC services to the services container.
+            services.AddMvc();
+
+            // Add Authentication services.
+            services.AddAuthentication(sharedOptions => sharedOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+
+            services.AddAuthorization(options =>
+            {
+                //foreach (var accessPolicy in new ArrayList { "policy1", "policy2" })
+                //{
+                //    options.AddPolicy(accessPolicy, policy => policy.Requirements.Add(new CustomRoleRequirement("myCustomRole")));
+                //}
+
+                options.AddPolicy("policy1", policy => policy.Requirements.Add(new CustomRoleRequirement("myCustomRole")));
+            });
+
+            services.AddSingleton<IAuthorizationHandler, CustomRoleHandler>();
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("Over21",
+            //                      policy => policy.Requirements.Add(new MinimumAgeRequirement(21)));
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // Add the console logger.
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            // Configure error handling middleware.
+            app.UseExceptionHandler("/Home/Error");
+
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            // Configure the OWIN pipeline to use cookie auth.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            // Configure the OWIN pipeline to use OpenID Connect auth.
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                //ClientId = Configuration["AzureAD:ClientId"],
+                //Authority = String.Format(Configuration["AzureAd:AadInstance"], Configuration["AzureAd:Tenant"]),
+                //ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                //GetClaimsFromUserInfoEndpoint=true,
+                //ClientSecret = Configuration["AzureAd:ClientSecret"],
+                //PostLogoutRedirectUri = Configuration["AzureAd:PostLogoutRedirectUri"],
+                //Events = new OpenIdConnectEvents
+                //{
+                //    OnRemoteFailure = OnAuthenticationFailed,
+                //    OnTokenValidated = context => {
+                //        //
+                //        Debug.WriteLine(context.Ticket.Principal.Claims.First(c=>c.Type==ClaimTypes.Upn));
+                //        return Task.FromResult(0); }
+                //},
+            
+
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                ClientId = Configuration["AzureAD:ClientId"],
+                Authority = String.Format(Configuration["AzureAd:AadInstance"], Configuration["AzureAd:Tenant"]),
+                ClientSecret = Configuration["AzureAd:ClientSecret"],
+                CallbackPath = new PathString("/signin-oidc"),
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                GetClaimsFromUserInfoEndpoint = true,
+                Events = new OpenIdConnectEvents
+                {
+                    OnAuthenticationFailed = OnAuthenticationFailed,
+                    OnAuthorizationCodeReceived = OnAuthorizationCodeReceived,
+                    OnMessageReceived = OnMessageReceived,
+                    OnTicketReceived = OnTicketRecieved,
+                    OnTokenValidated = OnTokenValidated,
+                    OnUserInformationReceived = OnUserInformationReceived,
+                    OnTokenResponseReceived = OnTokenResponseRecieved,
+                    OnRemoteFailure = OnRemoteFailure
+                }
+
+            });
+
+            // Configure MVC routes
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        // Handle sign-in errors differently than generic errors.
+        //private Task OnAuthenticationFailed(FailureContext context)
+        //{
+        //    context.HandleResponse();
+        //    context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+        //    return Task.FromResult(0);
+        //}
+
+        private Task OnRemoteFailure(FailureContext context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+            return Task.FromResult(0);
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedContext context)
+        {
+            return Task.FromResult(0);
+        }
+
+        private Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedContext context)
+        {
+            //context.Ticket.Principal.Claims.ad
+            return Task.FromResult(0);
+        }
+
+        private Task OnMessageReceived(MessageReceivedContext context)
+        {
+            return Task.FromResult(0);
+        }
+
+        private Task OnTicketRecieved(TicketReceivedContext context)
+        {
+            return Task.FromResult(0);
+        }
+
+        private Task OnTokenValidated(TokenValidatedContext context)
+        {
+           ((ClaimsIdentity)context.Ticket.Principal.Identity).AddClaim(new Claim("myClaim", "myClaimValue"));
+            return Task.FromResult(0);
+        }
+        private Task OnUserInformationReceived(UserInformationReceivedContext context)
+        {
+            return Task.FromResult(0);
+        }
+
+        private Task OnTokenResponseRecieved(TokenResponseReceivedContext context)
+        {
+            return Task.FromResult(0);
+        }
+
+    }
+    public class ClaimsTransformer : IClaimsTransformer
+    {
+        //Task<ClaimsPrincipal> TransformAsync(ClaimsTransformationContext context);
+
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsTransformationContext context)
+        {
+            ((ClaimsIdentity)context.Principal.Identity).AddClaim(new Claim("myClaim", "myClaimValue"));
+            //((ClaimsIdentity)principal.Identity).AddClaim(new Claim("ProjectReader", "true"));
+            return Task.FromResult(context.Principal);
+        }
+    }
+
+    public class MinimumAgeRequirement : IAuthorizationRequirement
+    {
+        public MinimumAgeRequirement(int age)
+        {
+            MinimumAge = age;
+        }
+
+        protected int MinimumAge { get; set; }
+    }
+
+    class CustomRoleRequirement : IAuthorizationRequirement
+    {
+        public CustomRoleRequirement(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public String RoleName{ get; set; }
+    }
+
+    class CustomRoleHandler : AuthorizationHandler<CustomRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomRoleRequirement requirement)
+        {
+            //if (context.User.HasClaim(claim => { return claim.Subject.Name == "roles"; }))
+            //{
+            //    var roles = context.User.FindFirst(claim => { return claim.Subject.Name == "roles"; });
+
+            //    if (roles.Value.Split(new char[] { ',', ';' }).Contains(requirement.RoleName))
+            //        context.Succeed(requirement);
+            //}
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+           
+        }
+    }
+}
